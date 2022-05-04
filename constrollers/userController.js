02d2bc9070ff40b257fdcbe78ad35467d0851230
Shubham Garg <57904338+shubhamgarg1671@users.noreply.github.com>
2022-05-04 18:04:01 +0530

    minor changes

--- constrollers/userController.js
+++ constrollers/userController.js
@@ -31,39 +31,3 @@ module.exports.login = async (req, res) => {
     });
 };
 
-module.exports.SocialSignup = async (req, res) => {
-    var uid = _.pick(req.body, ['uid']);
-
-	const newUser = await User.create({
-		name: decoded.name,
-		email: email,
-		profilePic: decoded.picture,
-		isGoogle: true,
-		role: role,
-	});
-
-	const token = jwt.sign(
-		{
-			_id: newUser._id,
-			role: newUser.role,
-x       		},
-		process.env.JWT_SECRET,
-		{
-			expiresIn: '30d',
-		}
-	);
-	await Notification.create({
-		for: 'admin',
-		type: 'New Registration',
-		message: `New ${role}, ${decoded.name} has just signed up.`,
-	});
-
-	return res.status(200).json({
-		status: '200',
-		message: 'User Registered Successfully',
-		token: token,
-		role: role,
-		userID: newUser._id,
-		activeSub: newUser.activeSub,
-	});
-};
