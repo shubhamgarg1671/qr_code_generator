a350716f93c8f72c8c29b6f3ccbfdd7bbd27ae63
Shubham Garg <57904338+shubhamgarg1671@users.noreply.github.com>
2022-05-08 16:26:39 +0530

    qrType fields added

--- model/qrType.js
+++ model/qrType.js
@@ -4,7 +4,78 @@ var qrTypeSchema = new mongoose.Schema({
     cryptocurrency:String,
     amount:Number,
     receiverBitcoinAddress:String,
-    message:String
+    message:String,
+    colorPrimary:String,
+    colorButton:String,
+    colorGradiant:String,
+    useColorGradiant:Boolean,
+    imageString:String,
+    couponCompany:String,
+    couponHeadline:String,
+    couponDescription:String,
+    saleBadge:String,
+    couponName:String,
+    couponValidUntil:String,
+    couponTNC:String,
+    website:String,
+    streetAddress:String,
+    numberAddrees:String,
+    cityAddrees:String,
+    stateAddrees:String,
+    zipAddrees:String,
+    countryAddrees:String,
+    welcomeImage:String,
+    email:String,
+    emailSubject:String,
+    phoneNumber:String,
+    firstName:String,
+    lastName:String,
+    mobileNumber:String,
+    faxNumber:String,
+    headline:String,
+    aboutUs:String,
+    shareButtonActive:Boolean,
+    text:String,
+    isTweetOrProfile:Boolean,
+    socialLinks:{
+        type:[
+            {name:String,
+            description:String}
+        ]
+    },
+    company:String,
+    job:String,
+    summary:String,
+    networkName:String,
+    password:String,
+    encryption:String,
+    button:{
+        type: [{
+            name:String,
+            link:String
+        }]
+    },
+    openingHours :{
+        type:[{
+            day:String,
+            openTime:String,
+            closeTime:String
+        }]
+    },
+    features:{
+        type:[{
+            name:String,
+            isAvailable:Boolean
+        }]
+    },
+    catagories:{
+        type:[{
+            name:String
+        }]
+    },
+    question:String,
+    ratingStyle:String,
+    videoURL:String
 }, {
     timestamps:true
 })
--- model/qrType/socialMediaQR.js
+++ model/qrType/socialMediaQR.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
 
 var socialMediaQrSchema = new mongoose.Schema({
-    creator: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref:'Qr'
-    },
-    socialMediaColorPrimary:String,
-    socialMediaColorButton:String,
-    socialMediaImageString:String,
     socialMediaHeadline:String,
     socialMediaAboutUs:String,
     socialLinks:{
